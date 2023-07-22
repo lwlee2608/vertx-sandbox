61d53c6c07ee77218b5251b16276797549c577be
Jason Lee <lwlee2608@gmail.com>
2023-07-22 22:21:25 +0800

    make vertx-codegen-protobuf works with lombok

--- vertx-codegen-sandbox/src/main/java/io/github/lwlee2608/vertx/sandbox/codegen/pojo/PeerEndpoint.java
+++ vertx-codegen-sandbox/src/main/java/io/github/lwlee2608/vertx/sandbox/codegen/pojo/PeerEndpoint.java
@@ -4,7 +4,7 @@ import io.vertx.codegen.annotations.DataObject;
 import io.vertx.codegen.protobuf.annotations.ProtobufGen;
 import lombok.Data;
 
-//@Data
+@Data
 @DataObject
 @ProtobufGen
 public class PeerEndpoint {
@@ -13,36 +13,4 @@ public class PeerEndpoint {
     //private TransportType transportType;
     private String host;
     private Integer port;
-
-    public Integer getSchemaVersion() {
-        return schemaVersion;
-    }
-
-    public void setSchemaVersion(Integer schemaVersion) {
-        this.schemaVersion = schemaVersion;
-    }
-
-    public Integer getExtVersion() {
-        return extVersion;
-    }
-
-    public void setExtVersion(Integer extVersion) {
-        this.extVersion = extVersion;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public Integer getPort() {
-        return port;
-    }
-
-    public void setPort(Integer port) {
-        this.port = port;
-    }
 }
