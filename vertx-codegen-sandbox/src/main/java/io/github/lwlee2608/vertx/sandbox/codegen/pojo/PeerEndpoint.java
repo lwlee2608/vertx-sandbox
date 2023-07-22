3ca6601dc1fa1d12709cc32e886171685fa5be48
Jason Lee <lwlee2608@gmail.com>
2023-07-22 10:09:30 +0800

    fix protobuf converter not getting generated

--- vertx-codegen-sandbox/src/main/java/io/github/lwlee2608/vertx/sandbox/codegen/pojo/PeerEndpoint.java
+++ vertx-codegen-sandbox/src/main/java/io/github/lwlee2608/vertx/sandbox/codegen/pojo/PeerEndpoint.java
@@ -1,12 +1,11 @@
 package io.github.lwlee2608.vertx.sandbox.codegen.pojo;
 
 import io.vertx.codegen.annotations.DataObject;
-import io.vertx.codegen.annotations.ProtobufGen;
+import io.vertx.codegen.protobuf.annotations.ProtobufGen;
 import lombok.Data;
 
-@Data
+//@Data
 @DataObject
-//@DataObject(generateConverter = true)
 @ProtobufGen
 public class PeerEndpoint {
     private Integer schemaVersion;
@@ -14,4 +13,36 @@ public class PeerEndpoint {
     //private TransportType transportType;
     private String host;
     private Integer port;
+
+    public Integer getSchemaVersion() {
+        return schemaVersion;
+    }
+
+    public void setSchemaVersion(Integer schemaVersion) {
+        this.schemaVersion = schemaVersion;
+    }
+
+    public Integer getExtVersion() {
+        return extVersion;
+    }
+
+    public void setExtVersion(Integer extVersion) {
+        this.extVersion = extVersion;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
 }
