a1ab0a027377c36f8aa4df8c1087ee87b4bedb5b
Jason Lee <lwlee2608@gmail.com>
2023-08-06 16:07:42 +0800

    use lombok

--- vertx-codegen-sandbox/build.gradle.kts
+++ vertx-codegen-sandbox/build.gradle.kts
@@ -7,4 +7,5 @@ dependencies {
     annotationProcessor("io.vertx:vertx-codegen-protobuf:${property("vertx.version")}")
     implementation("io.vertx:vertx-core:${property("vertx.version")}")
     implementation("io.vertx:vertx-codegen-protobuf:${property("vertx.version")}")
+    compileOnly("org.projectlombok:lombok:${property("lombok.version")}")
 }
